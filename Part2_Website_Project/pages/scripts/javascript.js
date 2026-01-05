95fadcb1f5aa30677c5d663e2a6bf396f653eaa8
Nader-cse <mnn400671@gu.edu.eg>
2026-01-05 18:54:17 +0200

    update

--- Part2_Website_Project/pages/scripts/javascript.js
+++ Part2_Website_Project/pages/scripts/javascript.js
@@ -3,7 +3,8 @@
 class EventManager {
   constructor(containerId) {
     this.container = document.getElementById(containerId);
-    this.events = [
+    this.events = 
+    [
       { name: "Tech Conference 2026", date: "2026-02-15", location: "Cairo", price: 1200 },
       { name: "Music Festival", date: "2026-03-10", location: "Alexandria", price: 800 },
       { name: "Startup Meetup", date: "2026-01-25", location: "Giza", price: 300 }
@@ -13,7 +14,8 @@ class EventManager {
 
   renderEvents(eventsList) {
     this.container.innerHTML = "";
-    eventsList.forEach(event => {
+    eventsList.forEach(event => 
+    {
       const card = document.createElement("div");
       card.className = "event-card";
       card.innerHTML = `
